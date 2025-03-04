8ebe12f4ce6f49516df303742e2893316289dbd6
zuqorl <alextheo482@gmail.com>
2025-03-04 10:59:36 +0100

    Update KeySystem-(bêta).lua

--- KeySystem-(bêta).lua
+++ KeySystem-(bêta).lua
@@ -4,7 +4,6 @@ local response = KeySystem:Init({
 	Debug=false, -- <bool> Prints some output in console when true
 	Title="CDTStorm bêta", -- <string or nil> Title of key system
 	Description=nil, -- <string or nil> Description of key system
-	Link="", -- <string> String to get key
 	Discord="https://discord.gg/wS5JgH3dV6", -- <string or nil> Button to join your discord server
 	SaveKey=false, -- <bool or nil> Just auto save key
 	Verify=function(key) -- <function> Verify is key valid
