1f12f887fea6caa83d2ecd94c39e72c7521d84a0
zuqorl <alextheo482@gmail.com>
2025-03-04 10:54:38 +0100

    Create UiTemplate.lua

---
+++ UiTemplate.lua
@@ -0,0 +1,5 @@
+-- Load the DrRay library from the GitHub repository Library
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+
+-- Create a new window and set its title and theme
+local window = DrRayLibrary:Load("CDTStorm", "Default")
